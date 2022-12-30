c502c797a9322e86c0226a2147ccafa67f1eb172
onur <onur.catmabacak@spherene.ch>
2022-12-30 13:18:26 +0000

    30.12.2022

--- test.py
+++ test.py
@@ -26,6 +26,60 @@ def save_mesh_open3d(filename):
     o3d.io.write_triangle_mesh(mesh, "bunny_saved_open3d.obj")
 
 
+def open3d_wireframe(filename):
+
+    mesh = load_mesh_open3d(filename) 
+    wireframe = o3d.geometry.LineSet.create_from_triangle_mesh(mesh)
+    o3d.visualization.draw_geometries([wireframe])
+
+
+def trimesh_to_open3d(filename, classification=True):
+
+    # To load and clean up mesh - "remove vertices that share position"
+
+    classification = False
+    if classification:
+        mesh_ = tri.load_mesh(filename, process=True)
+        mesh_.remove_duplicate_faces()
+    else:
+        mesh_ = tri.load_mesh(filename, process=False)
+        mesh = o3d.geometry.TriangleMesh()
+        mesh.vertices = o3d.utility.Vector3dVector(mesh_.vertices)
+        mesh.triangles = o3d.utility.Vector3iVector(mesh_.faces)
+        mesh.compute_vertex_normals()
+
+    return mesh
+
+
+def open3d_to_trimesh(filename):
+
+    mesh = load_mesh_open3d(filename)
+    triangle_normals = mesh.compute_triangle_normals() 
+    vertex_normals = mesh.compute_vertex_normals()
+    mesh = tri.Trimesh(vertices=mesh.vertices,faces=mesh.triangles, \
+    triangle_normals=triangle_normals, vertex_normals=vertex_normals)
+
+    return mesh
+
+def tetrahedral_mesh(filename):
+
+    mesh = load_mesh_open3d(filename)
+    mesh.compute_vertex_normals()
+    pcd = mesh.sample_points_poisson_disk(3000)
+    tetramesh = o3d.geometry.TetraMesh.create_from_point_cloud(pcd)
+    
+    return tetramesh
+
+
+    
+    
+
+    
+
 filename = "bunny.obj"
-save_mesh_open3d(filename)
-save_mesh_trimesh(filename)
+#save_mesh_open3d(filename)
+#save_mesh_trimesh(filename)
+#open3d_wireframe(filename)
+
+print(trimesh_to_open3d(filename))
+#print(open3d_to_trimesh(filename))
