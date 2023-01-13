df9bda0696d91a83d919b409b24dd566fb052110
onur <onur.catmabacak@spherene.ch>
2023-01-13 10:30:27 +0000

    updated

--- test.py
+++ test.py
@@ -71,15 +71,54 @@ def tetrahedral_mesh(filename):
     return tetramesh
 
 
-    
-    
 
-    
 
+def trimesh_to_open3d(filename, classification=True):
+
+    # To load and clean up mesh - "remove vertices that share position"
+
+	classification = False
+	if classification:
+		mesh_ = tri.load_mesh(filename, process=True)
+		mesh_.remove_duplicate_faces()
+	else:
+		mesh_ = tri.load_mesh(filename, process=False)
+		mesh = o3d.geometry.TriangleMesh()
+		mesh.vertices = o3d.utility.Vector3dVector(mesh_.vertices)
+		mesh.triangles = o3d.utility.Vector3iVector(mesh_.faces)
+		mesh.compute_vertex_normals()
+		
+		o3d.visualization.draw_geometries([mesh])
+		
+		return mesh
+		
+def trimesh_to_open3d_as_open(filename):
+
+	mesh = tri.load_mesh(filename)
+	print(tri.base.Trimesh.as_open3d(mesh))
+		
+def form_mesh_open3d(filename): 
+
+	mesh_tri = tri.load_mesh(filename)
+	mesh = o3d.geometry.TriangleMesh()
+	mesh.vertices = o3d.utility.Vector3dVector(mesh_tri.vertices)
+	mesh.triangles = o3d.utility.Vector3iVector(mesh_tri.faces)
+	mesh.compute_vertex_normals()
+	print(mesh.get_axis_aligned_bounding_box())
+	#print(mesh.is_self_intersecting())
+
+	mesh3d = o3d.geometry.VoxelGrid.create_from_triangle_mesh(mesh, 10.0)
+
+	o3d.visualization.draw_geometries([mesh3d])
+    
+    
 filename = "bunny.obj"
 #save_mesh_open3d(filename)
 #save_mesh_trimesh(filename)
 #open3d_wireframe(filename)
-
-print(trimesh_to_open3d(filename))
-#print(open3d_to_trimesh(filename))
+#tetrahedral_mesh(filename)
+#trimesh_to_open3d(filename, classification=True)
+#delete_get_set_attributes_open3d(filename)
+#trimesh_to_open3d_as_open(filename)
+form_mesh_open3d(filename)    
+    
